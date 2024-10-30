23d3b76e9708631b7ff6a83b946db743d8fda4ac
aadityaverma2011 <112503547+aadityaverma2011@users.noreply.github.com>
2024-10-30 21:27:15 +0530

    edit button facilities only visible to specific roles

--- src/Modules/Department/Components/Facilities.jsx
+++ src/Modules/Department/Components/Facilities.jsx
@@ -1,4 +1,5 @@
 import React, { lazy } from "react";
+import { useSelector } from "react-redux";
 import SpecialTable from "./SpecialTable";
 import studentData from "./Data/Data";
 
@@ -20,12 +21,40 @@ const columns = [
 ];
 
 function Facilities() {
+  // Access user role from Redux
+  const role = useSelector((state) => state.user.role);
+
   // Filter only CSE department labs
   const cseLabs = studentData.labs.filter((lab) => lab.department === "CSE");
 
   return (
     <div>
       <FacilitiesDescriptive />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          marginBottom: "10px",
+          marginTop: "10px",
+        }}
+      >
+        {/* Conditionally render the Edit button for 'dept_admin' and 'HOD (CSE)' roles */}
+        {(role === "dept_admin" || role === "HOD (CSE)") && (
+          <button
+            style={{
+              padding: "5px 20px",
+              backgroundColor: "indigo",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            Edit
+          </button>
+        )}
+      </div>
       <SpecialTable
         title="Labs"
         columns={columns}
