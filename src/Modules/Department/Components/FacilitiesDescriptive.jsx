ef57c90921ed2f7799a32e60ad22461a01594e2a
aadityaverma2011 <112503547+aadityaverma2011@users.noreply.github.com>
2024-11-01 02:04:02 +0530

    facilities fetch working

--- src/Modules/Department/Components/FacilitiesDescriptive.jsx
+++ src/Modules/Department/Components/FacilitiesDescriptive.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function FacilitiesDescriptive() {
+  const [data, setData] = useState({
+    phone_number: "",
+    email: "",
+    facilites: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:8000/dep/api/information/",
+        );
+        const result = await response.json();
+
+        // Assuming result is structured as an object with phone_number, email, and facilites fields
+        setData({
+          phone_number: result.phone_number || "NA",
+          email: result.email || "NA",
+          facilites: result.facilites || "NA",
+        });
+      } catch (error) {
+        console.error("Error fetching information data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div
       style={{
         backgroundColor: "white",
-        width: "800px", // Adjust the width as per your needs
+        width: "800px",
         padding: "20px",
         border: "1px solid #e0e0e0",
         borderRadius: "8px",
       }}
     >
       <div style={{ marginBottom: "20px" }}>
-        <p style={{ fontSize: "14px", color: "#000" }}>Phone Number.</p>
+        <p style={{ fontSize: "14px", color: "#000" }}>
+          Phone Number: {data.phone_number}
+        </p>
         <div
           style={{
             height: "1px",
@@ -22,7 +52,7 @@ export default function FacilitiesDescriptive() {
         />
       </div>
       <div style={{ marginBottom: "20px" }}>
-        <p style={{ fontSize: "14px", color: "#000" }}>Email:</p>
+        <p style={{ fontSize: "14px", color: "#000" }}>Email: {data.email}</p>
         <div
           style={{
             height: "1px",
@@ -33,7 +63,7 @@ export default function FacilitiesDescriptive() {
       </div>
       <div>
         <p style={{ fontSize: "14px", color: "#000" }}>
-          Facilities Description :
+          Facilities Description: {data.facilites}
         </p>
         <div
           style={{
