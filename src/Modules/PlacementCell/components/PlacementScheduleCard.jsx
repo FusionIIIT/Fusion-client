7017c7dc3e4e851548dd3241a8c3a97fec9903ae
abhijayrawat <128585063+abhijayrawat@users.noreply.github.com>
2024-11-13 10:25:00 +0530

    added the modal form for editing the placement event - abhijayrawat

--- src/Modules/PlacementCell/components/PlacementScheduleCard.jsx
+++ src/Modules/PlacementCell/components/PlacementScheduleCard.jsx
@@ -1,10 +1,175 @@
+// import React, { useState } from "react";
+// import { Card, Text, Badge, Group, Button, Image, ActionIcon } from "@mantine/core";
+// import { Clock, MapPin, Trash, Pencil, Eye } from "@phosphor-icons/react";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector } from 'react-redux';
+
+// function PlacementScheduleCard({
+//   companyLogo,
+//   companyName,
+//   location,
+//   position,
+//   jobType,
+//   postedTime,
+//   deadline,
+//   description,
+//   salary,
+// }) {
+//   const role = useSelector((state) => state.user.role);
+
+//   const [visible, setVisible] = useState(true);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleApplyClick = () => {
+//     navigate(
+//       `/placement-cell/apply?companyName=${encodeURIComponent(companyName)}&companyLogo=${encodeURIComponent(companyLogo)}`,
+//     );
+//   };
+
+//   const handelViewClick = () => {
+//     navigate(
+//       `/placement-cell/view?companyName=${encodeURIComponent(companyName)}&companyLogo=${encodeURIComponent(companyLogo)}`,
+//     );
+//   };
+
+//   const handleDeleteClick = () => {
+//     setVisible(false);
+//   };
+
+//   const handleEditClick = () => {
+//     // Logic for editing the event can be added here
+
+//     alert("Edit button clicked!");
+//   };
+
+//   if (!visible) return null;
+
+//   return (
+//     <Card
+//       shadow="sm"
+//       padding="lg"
+//       radius="lg"
+//       withBorder
+//       style={{ maxWidth: 400, position: "relative" }}
+//     >
+
+//       {/* Logo and Company Information */}
+//       <Group align="flex-start">
+//         <Image
+//           src={companyLogo}
+//           alt={`${companyName} logo`}
+//           width={40}
+//           height={40}
+//           fit="contain"
+//           withPlaceholder
+//         />
+//       </Group>
+//       <Text weight={700} size="lg" mt={10}>
+//         {companyName}
+//       </Text>
+//       <Group spacing={5} mt={5}>
+//         <MapPin size={16} />
+//         <Text size="sm" color="dimmed">
+//           {location}
+//         </Text>
+//       </Group>
+//       <Text weight={500} size="md" mt="sm">
+//         {position}
+//       </Text>
+//       <Group mt="xs" spacing="xs">
+//         <Badge color="green">{jobType}</Badge>
+//         <Group spacing={5}>
+//           <Clock size={16} />
+//           <Text size="xs" color="dimmed">
+//             {postedTime}
+//           </Text>
+//         </Group>
+//       </Group>
+//       <Text
+//         size="sm"
+//         mt="sm"
+//         color="dimmed"
+//         style={{
+//           overflow: "hidden",
+//           textOverflow: "ellipsis",
+//           whiteSpace: "nowrap",
+//         }}
+//       >
+//         {description}
+//       </Text>
+//       <Group position="apart" mt="md" style={{ display: 'flex', justifyContent: 'space-between' }}>
+//         <Text size="xl" weight={700} color="blue">
+//           {salary}
+//         </Text>
+
+//         {/* Apply now button for students only */}
+
+//         {role == 'student' && (
+//           <Button
+//             variant="light"
+//             color="blue"
+//             size="xs"
+//             onClick={handleApplyClick}
+//           >
+//             Apply Now
+//           </Button>
+//         )
+//         }
+
+//         {/* Delete and Edit icons */}
+//         {role == 'placement officer' && (
+
+//           <Group position="right" spacing="xs" style={{}}>
+//             <ActionIcon onClick={handleEditClick} color="blue" size="md" variant="light">
+//               <Pencil size={22} />
+//             </ActionIcon>
+
+//             <ActionIcon onClick={handelViewClick} color="blue" size="md" variant="light">
+//               <Eye size={22} />
+//             </ActionIcon>
+
+//             <ActionIcon onClick={handleDeleteClick} color="red" size="md" variant="light">
+//               <Trash size={22} />
+//             </ActionIcon>
+//           </Group>
+//         )}
+
+//       </Group>
+//     </Card>
+//   );
+// }
+
+// export default PlacementScheduleCard;
 
 import React, { useState } from "react";
-import { Card, Text, Badge, Group, Button, Image, ActionIcon } from "@mantine/core";
-import { Clock, MapPin, Trash, Pencil, Eye } from "@phosphor-icons/react";
+import {
+  Card,
+  Text,
+  Badge,
+  Group,
+  Button,
+  Image,
+  ActionIcon,
+  Modal,
+  TextInput,
+  Select,
+  Textarea,
+  Grid,
+  Title,
+} from "@mantine/core";
+import {
+  Clock,
+  MapPin,
+  Trash,
+  Pencil,
+  Eye,
+  Calendar,
+} from "@phosphor-icons/react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
+import { DatePicker, TimeInput } from "@mantine/dates";
+import PropTypes from "prop-types";
 
 function PlacementScheduleCard({
   companyLogo,
@@ -13,14 +178,24 @@ function PlacementScheduleCard({
   position,
   jobType,
   postedTime,
-  deadline,
+  // deadline,
   description,
   salary,
 }) {
   const role = useSelector((state) => state.user.role);
-
   const [visible, setVisible] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
+
+  const [company, setCompany] = useState(companyName);
+  const [date, setDate] = useState(null);
+  const [datePickerOpened, setDatePickerOpened] = useState(false);
+  const [locationInput, setLocation] = useState(location);
+  const [ctc, setCtc] = useState("");
+  const [time, setTime] = useState("");
+  const [placementType, setPlacementType] = useState("");
+  const [jobRole, setRole] = useState(position);
+  const [descriptionInput, setDescription] = useState(description);
 
   const handleApplyClick = () => {
     navigate(
@@ -39,107 +214,238 @@ function PlacementScheduleCard({
   };
 
   const handleEditClick = () => {
-    // Logic for editing the event can be added here
-    alert("Edit button clicked!");
+    setIsModalOpen(true);
+  };
+
+  const handleSubmit = () => {
+    // Logic to handle form submission
+    alert("Form submitted!");
+    setIsModalOpen(false); // Close modal after submission
   };
 
   if (!visible) return null;
 
   return (
-    <Card
-      shadow="sm"
-      padding="lg"
-      radius="lg"
-      withBorder
-      style={{ maxWidth: 400, position: "relative" }}
-    >
-
-
-      {/* Logo and Company Information */}
-      <Group align="flex-start">
-        <Image
-          src={companyLogo}
-          alt={`${companyName} logo`}
-          width={40}
-          height={40}
-          fit="contain"
-          withPlaceholder
-        />
-      </Group>
-      <Text weight={700} size="lg" mt={10}>
-        {companyName}
-      </Text>
-      <Group spacing={5} mt={5}>
-        <MapPin size={16} />
-        <Text size="sm" color="dimmed">
-          {location}
+    <>
+      <Card
+        shadow="sm"
+        padding="lg"
+        radius="lg"
+        withBorder
+        style={{ maxWidth: 400, position: "relative" }}
+      >
+        <Group align="flex-start">
+          <Image
+            src={companyLogo}
+            alt={`${companyName} logo`}
+            width={40}
+            height={40}
+            fit="contain"
+            withPlaceholder
+          />
+        </Group>
+        <Text weight={700} size="lg" mt={10}>
+          {companyName}
         </Text>
-      </Group>
-      <Text weight={500} size="md" mt="sm">
-        {position}
-      </Text>
-      <Group mt="xs" spacing="xs">
-        <Badge color="green">{jobType}</Badge>
-        <Group spacing={5}>
-          <Clock size={16} />
-          <Text size="xs" color="dimmed">
-            {postedTime}
+        <Group spacing={5} mt={5}>
+          <MapPin size={16} />
+          <Text size="sm" color="dimmed">
+            {location}
           </Text>
         </Group>
-      </Group>
-      <Text
-        size="sm"
-        mt="sm"
-        color="dimmed"
-        style={{
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          whiteSpace: "nowrap",
-        }}
-      >
-        {description}
-      </Text>
-      <Group position="apart" mt="md" style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Text size="xl" weight={700} color="blue">
-          {salary}
+        <Text weight={500} size="md" mt="sm">
+          {position}
         </Text>
-
-        {/* Apply now button for students only */}
-
-        {role == 'student' && (
-          <Button
-            variant="light"
-            color="blue"
-            size="xs"
-            onClick={handleApplyClick}
-          >
-            Apply Now
-          </Button>
-        )
-        }
-
-
-        {/* Delete and Edit icons */}
-        {role == 'placement officer' && (
-
-          <Group position="right" spacing="xs" style={{}}>
-            <ActionIcon onClick={handleEditClick} color="blue" size="md" variant="light">
-              <Pencil size={22} />
-            </ActionIcon>
- 
-            <ActionIcon onClick={handelViewClick} color="blue" size="md" variant="light">
-              <Eye size={22} />
-            </ActionIcon>
-
-            <ActionIcon onClick={handleDeleteClick} color="red" size="md" variant="light">
-              <Trash size={22} />
-            </ActionIcon>
+        <Group mt="xs" spacing="xs">
+          <Badge color="green">{jobType}</Badge>
+          <Group spacing={5}>
+            <Clock size={16} />
+            <Text size="xs" color="dimmed">
+              {postedTime}
+            </Text>
           </Group>
-        )}
+        </Group>
+        <Text
+          size="sm"
+          mt="sm"
+          color="dimmed"
+          style={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {description}
+        </Text>
+        <Group
+          position="apart"
+          mt="md"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Text size="xl" weight={700} color="blue">
+            {salary}
+          </Text>
 
-      </Group>
-    </Card>
+          {role === "student" && (
+            <Button
+              variant="light"
+              color="blue"
+              size="xs"
+              onClick={handleApplyClick}
+            >
+              Apply Now
+            </Button>
+          )}
+
+          {role === "placement officer" && (
+            <Group position="right" spacing="xs">
+              <ActionIcon
+                onClick={handleEditClick}
+                color="blue"
+                size="md"
+                variant="light"
+              >
+                <Pencil size={22} />
+              </ActionIcon>
+
+              <ActionIcon
+                onClick={handelViewClick}
+                color="blue"
+                size="md"
+                variant="light"
+              >
+                <Eye size={22} />
+              </ActionIcon>
+
+              <ActionIcon
+                onClick={handleDeleteClick}
+                color="red"
+                size="md"
+                variant="light"
+              >
+                <Trash size={22} />
+              </ActionIcon>
+            </Group>
+          )}
+        </Group>
+      </Card>
+
+      {/* Modal for editing form */}
+      <Modal
+        opened={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Edit Placement Event"
+      >
+        <Card shadow="md" padding="lg" radius="md" withBorder>
+          <Title order={3} align="center" style={{ marginBottom: "20px" }}>
+            Edit Placement Event
+          </Title>
+          <Grid gutter="lg">
+            <Grid.Col span={4}>
+              <TextInput
+                label="Company Name"
+                placeholder="Enter company name"
+                value={company}
+                onChange={(e) => setCompany(e.target.value)}
+              />
+            </Grid.Col>
+            <Grid.Col span={4} style={{ position: "relative" }}>
+              <TextInput
+                label="Date (yyyy-mm-dd)"
+                placeholder="Pick date"
+                value={date ? date.toLocaleDateString() : ""}
+                readOnly
+                rightSection={
+                  <ActionIcon
+                    onClick={() => setDatePickerOpened((prev) => !prev)}
+                  >
+                    <Calendar size={16} />
+                  </ActionIcon>
+                }
+              />
+              {datePickerOpened && (
+                <DatePicker
+                  value={date}
+                  onChange={(selectedDate) => {
+                    setDate(selectedDate);
+                    setDatePickerOpened(false);
+                  }}
+                  onBlur={() => setDatePickerOpened(false)}
+                  style={{ zIndex: 1 }}
+                />
+              )}
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <TextInput
+                label="Location"
+                placeholder="Enter location"
+                value={locationInput}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <TextInput
+                label="CTC In Lpa"
+                placeholder="Enter CTC"
+                value={ctc}
+                onChange={(e) => setCtc(e.target.value)}
+              />
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <TimeInput
+                label="Time"
+                placeholder="Select time"
+                value={time}
+                onChange={(value) =>
+                  setTime(value.toLocaleTimeString("en-GB", { hour12: false }))
+                }
+                format="24"
+              />
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Select
+                label="Placement Type"
+                placeholder="Select placement type"
+                data={["Placement", "Internship"]}
+                value={placementType}
+                onChange={setPlacementType}
+              />
+            </Grid.Col>
+            <Grid.Col span={12}>
+              <Textarea
+                label="Description"
+                placeholder="Enter a description"
+                value={descriptionInput}
+                onChange={(e) => setDescription(e.target.value)}
+                minRows={3}
+              />
+            </Grid.Col>
+            <Grid.Col span={12}>
+              <TextInput
+                label="Role Offered"
+                placeholder="Enter the role offered"
+                value={jobRole}
+                onChange={(e) => setRole(e.target.value)}
+              />
+            </Grid.Col>
+          </Grid>
+          <Group position="right" style={{ marginTop: "20px" }}>
+            <Button onClick={handleSubmit}>Save Changes</Button>
+          </Group>
+        </Card>
+      </Modal>
+    </>
   );
 }
-
+PlacementScheduleCard.propTypes = {
+  companyLogo: PropTypes.string,
+  companyName: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  jobType: PropTypes.string.isRequired,
+  postedTime: PropTypes.string.isRequired,
+  // deadline: PropTypes.string,
+  description: PropTypes.string,
+  salary: PropTypes.string,
+};
 export default PlacementScheduleCard;
--- src/Modules/PlacementCell/index.jsx
+++ src/Modules/PlacementCell/index.jsx
@@ -1,43 +1,92 @@
 import React from "react";
-import { Text, Tabs, Container } from "@mantine/core";
+import { Text, Tabs } from "@mantine/core";
 // import AddPlacementRecordForm from "./components/AddPlacementRecordForm";
-// import AddPlacementEventForm from "./components/AddPlacementEventForm";
-import PlacementRecordsTable from "./components/PlacementRecordsTable";
 import { useSelector } from "react-redux";
+import AddPlacementEventForm from "./components/AddPlacementEventForm";
+import PlacementRecordsTable from "./components/PlacementRecordsTable";
 import PlacementCalendar from "./components/PlacementCalendar";
 import PlacementSchedule from "./components/PlacementSchedule";
 import SendNotificationForm from "./components/SendNotificationForm";
 import DownloadCV from "./components/DownloadCV";
 
 const studentTabs = [
-  { value: "schedule", label: "Placement Schedule", component: <PlacementSchedule /> },
-  { value: "stats", label: "Placement Stats", component: <PlacementRecordsTable /> },
+  {
+    value: "schedule",
+    label: "Placement Schedule",
+    component: <PlacementSchedule />,
+  },
+  {
+    value: "stats",
+    label: "Placement Stats",
+    component: <PlacementRecordsTable />,
+  },
   { value: "download-cv", label: "Download CV", component: <DownloadCV /> },
-  { value: "placement-calendar", label: "Placement Calendar", component: <PlacementCalendar /> },
+  {
+    value: "placement-calendar",
+    label: "Placement Calendar",
+    component: <PlacementCalendar />,
+  },
 ];
 
 const chairmanTabs = [
-  { value: "schedule", label: "Placement Schedule", component: <PlacementSchedule /> },
-  { value: "stats", label: "Placement Stats", component: <PlacementRecordsTable /> },
-  { value: "placement-calendar", label: "Placement Calendar", component: <PlacementCalendar /> },
+  {
+    value: "schedule",
+    label: "Placement Schedule",
+    component: <PlacementSchedule />,
+  },
+  {
+    value: "stats",
+    label: "Placement Stats",
+    component: <PlacementRecordsTable />,
+  },
+  {
+    value: "placement-calendar",
+    label: "Placement Calendar",
+    component: <PlacementCalendar />,
+  },
 ];
 
 const tpoTabs = [
-  { value: "schedule", label: "Placement Schedule", component: <PlacementSchedule /> },
-  { value: "send-notifications", label: "Send Notifications", component: <SendNotificationForm /> },
-  { value: "stats", label: "Placement Stats", component: <PlacementRecordsTable /> },
+  {
+    value: "schedule",
+    label: "Placement Schedule",
+    component: <PlacementSchedule />,
+  },
+  {
+    value: "send-notifications",
+    label: "Send Notifications",
+    component: <SendNotificationForm />,
+  },
+  {
+    value: "stats",
+    label: "Placement Stats",
+    component: <PlacementRecordsTable />,
+  },
   // { value: "add-record", label: "Add Placement Record", component: <AddPlacementRecordForm /> },
-  { value: "placement-calendar", label: "Placement Calendar", component: <PlacementCalendar /> },
-  // { value: "add-event", label: "Add Placement Event", component: <AddPlacementEventForm /> },
+  {
+    value: "placement-calendar",
+    label: "Placement Calendar",
+    component: <PlacementCalendar />,
+  },
+  {
+    value: "add-event",
+    label: "Add Placement Event",
+    component: <AddPlacementEventForm />,
+  },
 ];
 
 function PlacementCellPage() {
   const role = useSelector((state) => state.user.role);
 
-  const tabs = role === "student" ? studentTabs
-    : role === "placement chairman" ? chairmanTabs
-      : role === "placement officer" ? tpoTabs
-        : [];
+  const tabs =
+    role === "student"
+      ? studentTabs
+      : role === "placement chairman"
+        ? chairmanTabs
+        : role === "placement officer"
+          ? tpoTabs
+          : [];
+  // const tabs = role ==="placement officer" ? tpoTabs:[];
 
   return (
     <div style={{ padding: "20px" }}>
@@ -51,17 +100,12 @@ function PlacementCellPage() {
         </Tabs.List>
 
         {tabs.map((tab) => (
-
           <Tabs.Panel key={tab.value} value={tab.value}>
             {tab.component || <Text>No content available.</Text>}
           </Tabs.Panel>
         ))}
-
-
       </Tabs>
-
     </div>
-
   );
 }
 
