44298f49f511aa00bc059cf777bc188578c0b6c1
Doraemon012 <neyati28@gmail.com>
2024-11-19 23:21:22 +0530

    fix: fixed index.jsx

--- src/Modules/PlacementCell/index.jsx
+++ src/Modules/PlacementCell/index.jsx
@@ -1,226 +1,13 @@
-// import "@mantine/notifications/styles.css";
-// import React, { useState, useRef } from "react";
-// import { Tabs, Button } from "@mantine/core";
-// import { useSelector } from "react-redux";
-// import { CaretCircleLeft, CaretCircleRight } from "@phosphor-icons/react";
-// import PlacementRecordsTable from "./components/PlacementRecordsTable";
-// import PlacementCalendar from "./components/PlacementCalendar";
-// import PlacementSchedule from "./components/PlacementSchedule";
-// import SendNotificationForm from "./components/SendNotificationForm";
-// import DownloadCV from "./components/DownloadCV";
-
-// const studentTabs = [
-//   {
-//     value: "schedule",
-//     label: "Placement Schedule",
-//     component: <PlacementSchedule />,
-//   },
-//   {
-//     value: "stats",
-//     label: "Placement Stats",
-//     component: <PlacementRecordsTable />,
-//   },
-//   { value: "download-cv", label: "Download CV", component: <DownloadCV /> },
-//   {
-//     value: "placement-calendar",
-//     label: "Placement Calendar",
-//     component: <PlacementCalendar />,
-//   },
-// ];
-
-// const chairmanTabs = [
-//   {
-//     value: "schedule",
-//     label: "Placement Schedule",
-//     component: <PlacementSchedule />,
-//   },
-//   {
-//     value: "stats",
-//     label: "Placement Stats",
-//     component: <PlacementRecordsTable />,
-//   },
-//   {
-//     value: "placement-calendar",
-//     label: "Placement Calendar",
-//     component: <PlacementCalendar />,
-//   },
-// ];
-
-// const tpoTabs = [
-//   {
-//     value: "schedule",
-//     label: "Placement Schedule",
-//     component: <PlacementSchedule />,
-//   },
-//   {
-//     value: "send-notifications",
-//     label: "Send Notifications",
-//     component: <SendNotificationForm />,
-//   },
-//   {
-//     value: "stats",
-//     label: "Placement Stats",
-//     component: <PlacementRecordsTable />,
-//   },
-//   {
-//     value: "placement-calendar",
-//     label: "Placement Calendar",
-//     component: <PlacementCalendar />,
-//   },
-// ];
-
-// const styles = {
-//   container: {
-//     padding: "20px",
-//   },
-//   navContainer: {
-//     display: "flex",
-//     alignItems: "center",
-//     gap: "0.5rem",
-//   },
-//   tabsContainer: {
-//     display: "flex",
-//     flexWrap: "nowrap",
-//     overflowX: "auto",
-//     scrollbarWidth: "none", // Hides scrollbar in Firefox
-//     msOverflowStyle: "none", // Hides scrollbar in Internet Explorer
-//     marginLeft: "10px", // Reduced distance between the left arrow and the tabs
-//   },
-//   tabsList: {
-//     display: "flex",
-//     gap: "0px", // No gap between tabs
-//   },
-//   navButton: {
-//     border: "none",
-//     backgroundColor: "#f5f5f5",
-//     cursor: "pointer",
-//     fontSize: "1.75rem",
-//     padding: "8px",
-//     width: "50px",
-//     height: "50px",
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     borderRadius: "50%",
-//     boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-//   },
-
-//   fusionCaretCircleIcon: {
-//     fontSize: "2rem",
-//   },
-
-//   tab: {
-//     fontWeight: "normal",
-//     color: "#6c757d",
-//     padding: "10px 20px",
-//     cursor: "pointer", // Ensures tabs are clickable
-//   },
-//   activeTab: {
-//     backgroundColor: "#15abff10", // Light blue background for active tab
-//     color: "#15abff",
-//     // fontWeight: "bold",
-//     borderRadius: "4px",
-//   },
-//   tabContent: {
-//     marginTop: "20px",
-//   },
-// };
-
-// function PlacementCellPage() {
-//   const role = useSelector((state) => state.user.role);
-//   const [activeTab, setActiveTab] = useState("schedule");
-//   const tabsContainerRef = useRef(null);
-
-//   const tabs =
-//     role === "student"
-//       ? studentTabs
-//       : role === "placement chairman"
-//         ? chairmanTabs
-//         : role === "placement officer"
-//           ? tpoTabs
-//           : [];
-
-//   return (
-//     <div style={styles.container}>
-//       <div style={styles.navContainer}>
-//         {/* Left navigation button */}
-//         <Button
-//           onClick={() => handleTabChange("prev")}
-//           variant="default"
-//           p={0}
-//           style={{ border: "none" }}
-//         >
-//           <CaretCircleLeft
-//             style={styles.fusionCaretCircleIcon}
-//             weight="light"
-//           />
-//         </Button>
-
-//         {/* Tabs list */}
-//         <div
-//           className="fusionTabsContainer"
-//           style={styles.tabsContainer}
-//           ref={tabsContainerRef}
-//         >
-//           <Tabs value={activeTab} onTabChange={setActiveTab}>
-//             <Tabs.List style={styles.tabsList}>
-//               {tabs.map((tab) => (
-//                 <Tabs.Tab
-//                   key={tab.value}
-//                   value={tab.value}
-//                   style={{
-//                     ...styles.tab,
-//                     ...(activeTab === tab.value && styles.activeTab),
-//                   }}
-//                   onClick={() => setActiveTab(tab.value)}
-//                 >
-//                   {tab.label}
-//                 </Tabs.Tab>
-//               ))}
-//             </Tabs.List>
-//           </Tabs>
-//         </div>
-
-//         {/* Right navigation button */}
-//         <Button
-//           onClick={() => handleTabChange("next")}
-//           variant="default"
-//           p={0}
-//           style={{ border: "none" }}
-//         >
-//           <CaretCircleRight
-//             style={styles.fusionCaretCircleIcon}
-//             weight="light"
-//           />
-//         </Button>
-//       </div>
-
-//       {/* Tab content */}
-//       <div style={styles.tabContent}>
-//         {tabs.map((tab) =>
-//           tab.value === activeTab ? (
-//             <div key={tab.value}>{tab.component}</div>
-//           ) : null,
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default PlacementCellPage;
-
+import "@mantine/notifications/styles.css";
 import React, { useState, useRef } from "react";
 import { Tabs, Button } from "@mantine/core";
 import { useSelector } from "react-redux";
 import { CaretCircleLeft, CaretCircleRight } from "@phosphor-icons/react";
-import { useLocation } from "react-router-dom"; // Hook to get the current location
-import AddPlacementEventForm from "./components/AddPlacementEventForm";
 import PlacementRecordsTable from "./components/PlacementRecordsTable";
 import PlacementCalendar from "./components/PlacementCalendar";
 import PlacementSchedule from "./components/PlacementSchedule";
 import SendNotificationForm from "./components/SendNotificationForm";
 import DownloadCV from "./components/DownloadCV";
-import CustomBreadcrumbs from "../../components/Breadcrumbs"; // Import the Breadcrumbs component
 
 const studentTabs = [
   {
@@ -280,11 +67,6 @@ const tpoTabs = [
     label: "Placement Calendar",
     component: <PlacementCalendar />,
   },
-  {
-    value: "add-event",
-    label: "Add Placement Event",
-    component: <AddPlacementEventForm />,
-  },
 ];
 
 const styles = {
@@ -336,13 +118,11 @@ const styles = {
   activeTab: {
     backgroundColor: "#15abff10", // Light blue background for active tab
     color: "#15abff",
+    // fontWeight: "bold",
     borderRadius: "4px",
   },
   tabContent: {
     marginTop: "20px",
-  },
-  breadcrumbContainer: {
-    marginBottom: "20px", // Add some space between the breadcrumbs and the tabs
   },
 };
 
@@ -350,42 +130,18 @@ function PlacementCellPage() {
   const role = useSelector((state) => state.user.role);
   const [activeTab, setActiveTab] = useState("schedule");
   const tabsContainerRef = useRef(null);
-  const location = useLocation(); // Get the current location
 
-  // Determine which set of tabs to display based on user role
   const tabs =
     role === "student"
       ? studentTabs
       : role === "placement chairman"
-      ? chairmanTabs
-      : role === "placement officer"
-      ? tpoTabs
-      : [];
-
-  // Handle tab changes (prev and next navigation)
-  const handleTabChange = (direction) => {
-    const currentIndex = tabs.findIndex((tab) => tab.value === activeTab);
-    const newIndex =
-      direction === "prev"
-        ? Math.max(currentIndex - 1, 0)
-        : Math.min(currentIndex + 1, tabs.length - 1);
-
-    setActiveTab(tabs[newIndex].value);
-
-    tabsContainerRef.current.scrollBy({
-      left: direction === "next" ? 100 : -100,
-      behavior: "smooth",
-    });
-  };
+        ? chairmanTabs
+        : role === "placement officer"
+          ? tpoTabs
+          : [];
 
   return (
     <div style={styles.container}>
-      {/* Breadcrumbs */}
-      <div style={styles.breadcrumbContainer}>
-        <CustomBreadcrumbs />
-      </div>
-
-      {/* Navigation and Tab Content */}
       <div style={styles.navContainer}>
         {/* Left navigation button */}
         <Button
@@ -394,7 +150,10 @@ function PlacementCellPage() {
           p={0}
           style={{ border: "none" }}
         >
-          <CaretCircleLeft style={styles.fusionCaretCircleIcon} weight="light" />
+          <CaretCircleLeft
+            style={styles.fusionCaretCircleIcon}
+            weight="light"
+          />
         </Button>
 
         {/* Tabs list */}
@@ -429,19 +188,23 @@ function PlacementCellPage() {
           p={0}
           style={{ border: "none" }}
         >
-          <CaretCircleRight style={styles.fusionCaretCircleIcon} weight="light" />
+          <CaretCircleRight
+            style={styles.fusionCaretCircleIcon}
+            weight="light"
+          />
         </Button>
       </div>
 
       {/* Tab content */}
       <div style={styles.tabContent}>
-        {tabs.map(
-          (tab) =>
-            tab.value === activeTab && <div key={tab.value}>{tab.component}</div>
+        {tabs.map((tab) =>
+          tab.value === activeTab ? (
+            <div key={tab.value}>{tab.component}</div>
+          ) : null,
         )}
       </div>
     </div>
   );
 }
 
-export default PlacementCellPage
+export default PlacementCellPage;
