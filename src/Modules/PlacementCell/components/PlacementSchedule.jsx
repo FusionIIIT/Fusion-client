e47b6239ba201bd5e9c07c7d97276fbb8f232a81
Doraemon012 <neyati28@gmail.com>
2025-04-22 16:48:16 +0530

    fix: fixed schedule, restrictions

--- src/Modules/PlacementCell/components/PlacementSchedule.jsx
+++ src/Modules/PlacementCell/components/PlacementSchedule.jsx
@@ -18,6 +18,7 @@ import { fetchPlacementScheduleRoute } from "../../../routes/placementCellRoutes
 
 const getCookie = (name) => {
   const value = `; ${document.cookie}`;
+
   const parts = value.split(`; ${name}=`);
 
   if (parts.length === 2) return parts.pop().split(";").shift();
